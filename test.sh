29899e22fa0c12e5cfaafa7dca4c60f529f52437
Ignacio Heredia <iheredia@ifca.unican.es>
2024-07-01 15:22:13 +0200

    update

--- test.sh
+++ test.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
+# This is the file that is ran from Nomad, when we launch a "try-it" job from PAPI
 
-# echo hello
-# timeout 30s sleep 900 || ( [[ $? -eq 124 ]] && echo 'WARNING: Timeout reached, but thats OK' )
+DURATION="${DURATION:-10m}"
+UI_PORT="${UI_PORT:-8888}"
 
-echo "sleeping"
-sleep 10000000000
+git clone -b nomad https://github.com/ai4os/deepaas_ui
+cd deepaas_ui
 
+#  Defaut installation leads to:
+# ```
+# ERROR: Cannot uninstall 'blinker'. It is a distutils installed project and
+# thus we cannot accurately determine which files belong to it which
+# would lead to only a partial uninstall.
+# ```
+# So we need to add the ignore flag.
+# https://stackoverflow.com/questions/53807511/pip-cannot-uninstall-package-it-is-a-distutils-installed-project
+pip install -r requirements.txt --ignore-installed blinker
 
-# DURATION="${DURATION:-10m}"
-# UI_PORT="${UI_PORT:-8888}"
+nohup deep-start --deepaas &
+# sleep 10s to let `deepaas` start before launching the UI
+sleep 10
+# Use timeout to automatically kill the job after a given duration
+# We capture the timeout exit code (124) to return 0 instead, so that the task does not restart (job_type=batch)
+echo "Launching Gradio UI ..."
 
-# git clone -b nomad https://github.com/ai4os/deepaas_ui
-# cd deepaas_ui
-
-# #  Defaut installation leads to:
-# # ```
-# # ERROR: Cannot uninstall 'blinker'. It is a distutils installed project and
-# # thus we cannot accurately determine which files belong to it which
-# # would lead to only a partial uninstall.
-# # ```
-# # So we need to add the ignore flag.
-# # https://stackoverflow.com/questions/53807511/pip-cannot-uninstall-package-it-is-a-distutils-installed-project
-# pip install -r requirements.txt --ignore-installed blinker
-
-# nohup deep-start --deepaas &
-# # sleep 10s to let `deepaas` start before launching the UI
-# sleep 10
-# # Use timeout to automatically kill the job after a given duration
-# # We capture the timeout exit code (124) to return 0 instead, so that the task does not restart (job_type=batch)
-# timeout ${DURATION} python launch.py --api_url http://0.0.0.0:5000/ --ui_port ${UI_PORT}
-
-
-
-
-# timeout ${DURATION} python launch.py --api_url http://0.0.0.0:5000/ --ui_port ${UI_PORT} || ( [[ $? -eq 124 ]]
+## Test restart: python --> pthon
+timeout ${DURATION} pthon launch.py --api_url http://0.0.0.0:5000/ --ui_port ${UI_PORT} || [[ $? -eq 124 ]]
